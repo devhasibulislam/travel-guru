1f9b1b5e3a3396d21bbe1220279976c741a8b754
hasibulislam999 <hasib143sl@gmail.com>
2022-07-02 22:15:39 +0600

    routes added on app file & style determined

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Reset from './pages/Reset';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/reset' element={<Reset />} />
       </Routes>
     </div>
   );
